7e3990a149326a84a14ab69491c7bead8721d050
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-03-10 17:53:37 +0500

    done

--- controllers/productControl.js
+++ controllers/productControl.js
@@ -4,8 +4,14 @@ const productControl = {
         const products = await Products.find()
         res.json(products);
     },
-    createProduct: (req, res) => {
+    createProduct: async (req, res) => {
         const { product_id, title, images, description, content, colors, sizes, price } = req.body;
+        const product = await Products.findOne({ product_id: product_id });
+        if (product) {
+            return res.status(400).json({
+                msg: "This product already exists"
+            });
+        };
         const newProduct = new Products({
             product_id,
             title,
@@ -15,17 +21,30 @@ const productControl = {
             colors,
             sizes,
             price
-        })
-        res.json({ newProduct })
+        });
+        await newProduct.save()
+        res.json({ msg: 'Created a Product' })
     },
-    deleteProduct: (req, res) => {
-        res.json({ msg: 'Deleted a product' })
+    deleteProduct: async (req, res) => {
+        await Products.findByIdAndDelete(req.params.id)
+        res.json({ msg: 'Deleted a product Succesfully' })
     },
-    updateProduct: (req, res) => {
+    updateProduct: async (req, res) => {
+        const { title, images, description, content, colors, sizes, price } = req.body;
+        await Products.findOneAndUpdate({ _id: req.params.id }, {
+            title,
+            images,
+            description,
+            content,
+            colors,
+            sizes,
+            price
+        });
         res.json({ msg: 'Updated a product' })
     },
-    getProduct: (req, res) => {
-        res.json({ msg: 'Get a product' })
+    getProduct: async (req, res) => {
+        const product = await Products.findById(req.params.id)
+        res.json(product);
     },
 }
 
